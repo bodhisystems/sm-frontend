7f1f1984edb6747b22c6c4d5ba80150be9e06af5
Subash Maharjan <subash.maharjan@updater.com>
2018-05-11 15:40:37 -0400

    App renamed to Component

---
+++ packages/smhello/src/Component.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  provide() {
+    return {
+      /**
+       * Global Styles
+       */
+       '$globalStyle': {
+         emphasized: this.$style.emphasized
+       }
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+  /* import scss variables */
+  @import "assets/settings.scss";
+
+  /* text styles */
+  .emphasized {
+    font-weight: 500;
+    line-height: 1.5;
+    color: $base_color;
+  }
+</style>
