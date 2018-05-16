c9847b11d2ea04bb49f40d7556c9d4579198e7f0
Subash Maharjan <subash.maharjan@updater.com>
2018-05-16 15:04:52 -0400

    vue fixed header added

--- packages/smhello/src/components/shared/Header.vue
+++ packages/smhello/src/components/shared/Header.vue
@@ -1,25 +1,45 @@
 <template>
-  <!-- header
-  ================================================== -->
-  <header class="s-header">
+  <fixed-header :fixed.sync="isFixed" :threshold="100">
+    <header class="s-header" :class="{ 'is-light': isFixed, 'is-primary': !isFixed }">
+        <Logo />
+        <Navigation />
+        <a class="header-menu-toggle" href="#0"><span>Menu</span></a>
+    </header>
+  </fixed-header>
 
-      <Logo />
-
-      <Navigation />
-
-      <a class="header-menu-toggle" href="#0"><span>Menu</span></a>
-
-  </header> <!-- end s-header -->
 </template>
 
 <script>
+import FixedHeader from 'vue-fixed-header'
 import Logo from '../lib/Logo';
 import Navigation from '../lib/Navigation'
 
 export default {
+  data(){
+    return {
+        isFixed: true
+    }
+  },
   components: {
+    FixedHeader,
     Logo,
     Navigation
   },
 }
 </script>
+
+<style>
+  .s-header {
+    position: fixed;
+    width: 100%;
+    transition: background 0.15s ease-out;
+    background: transparent;
+    z-index: 10000;
+  }
+  .s-header.is-primary {
+    background: transparent;
+  }
+  .s-header.is-light {
+    background: #111;
+  }
+</style>
