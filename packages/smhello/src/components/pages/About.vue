113bbe3f277ce44d0be067a1a39b9b63b794ab15
Subash Maharjan <subash.maharjan@updater.com>
2018-06-04 10:54:19 -0400

    Layout updated for each page

--- packages/smhello/src/components/pages/About.vue
+++ packages/smhello/src/components/pages/About.vue
@@ -1,161 +1,25 @@
 <template>
-  <section id="about" class="s-about target-section">
-
-      <div class="row narrow section-intro has-bottom-sep">
-          <div class="col-full text-center">
-              <h3>{{ $t('content.about.title') }}</h3>
-              <h1>{{ $t('content.about.sub-title') }}</h1>
-              <p class="lead">{{ $t('content.about.lead') }}</p>
-          </div>
-      </div>
-
-      <div class="row about-content">
-
-          <div class="col-full tab-full left">
-
-              <h3>{{ $t('content.about.tab-full.title') }}</h3>
-              <img src="../../../static/images/hero-bg.jpg" />
-              <p>{{ $t('content.about.tab-full.para-1') }}</p>
-              <p>{{ $t('content.about.tab-full.para-2') }}</p>
-
-          </div>
-
-          <!-- <div class="col-six tab-full right">
-
-          </div> -->
-
-          <!-- <div class="col-six tab-full right">
-              <h3>I've Got Some skills.</h3>
-
-              <ul class="skill-bars">
-                  <li>
-                  <div class="progress percent90"><span>90%</span></div>
-                  <strong>HTML</strong>
-                  </li>
-                  <li>
-                  <div class="progress percent85"><span>85%</span></div>
-                  <strong>CSS</strong>
-                  </li>
-                  <li>
-                  <div class="progress percent70"><span>70%</span></div>
-                  <strong>Javascript</strong>
-                  </li>
-                  <li>
-                  <div class="progress percent95"><span>50%</span></div>
-                  <strong>PHP, JAVA, .NET</strong>
-                  </li>
-                  <li>
-                  <div class="progress percent75"><span>60%</span></div>
-                  <strong>Wordpress, Drupal, OrchardCMS, Liferay</strong>
-                  </li>
-              </ul>
-          </div> -->
-
-      </div> <!-- end about-content -->
-
-      <div class="row about-content about-content--buttons">
-
-          <div class="col-six tab-full left">
-              <a href="#0" class="btn btn--primary full-width">Download My CV</a>
-          </div>
-          <div class="col-six tab-full right">
-              <a href="#0" class="btn full-width">Hire Me Now</a>
-          </div>
-
-      </div> <!-- end about-content buttons -->
-
-      <div class="row about-content about-content--timeline">
-
-          <div class="col-full text-center">
-              <h3>My Work Experience.</h3>
-          </div>
-
-          <div v-for="(column, index) in experiencColumn" :key="index" class="col-six tab-full" :class="index == 0 ? 'left': 'right'">
-            <div v-for="data in column" class="timeline" :key="data.agency">
-              <div class="timeline__block">
-                  <div class="timeline__bullet"></div>
-                  <div class="timeline__header">
-                      <p class="timeline__timeframe">{{ data.timeframe.start }} - {{ data.timeframe.end }}</p>
-                      <h3>{{ data.agency }}</h3>
-                      <h5>{{ data.location }}</h5>
-                      <h6>{{ data.position }}</h6>
-                  </div>
-                  <div class="timeline__desc">
-                      <p>{{ data.description }}</p>
-                  </div>
-              </div>
-            </div>
-          </div>
-      </div> <!-- end about-content timeline -->
-
+  <section class="layout">
+    <Header />
+    <AboutContent />
+    <Footer />
   </section>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        experiencColumn: [[],[]],
-        experience: [
-          {
-            timeframe: {
-              start: "March 2018",
-              end: "Present"
-            },
-            agency: "Updater",
-            position: "Senior Frontend Engineer",
-            location: "Charlottesville, Virginia",
-            description: "Updater is a technology company that builds powerful tools to help people move. Updater's digital tools help people find and reserve trustworthy moving companies, connect home services, forward their mail, transfer utilities, update accounts, and much more – all in one easy-to-use platform."
-          },
-          {
-            timeframe: {
-              start: "December 2015",
-              end: "March 2018"
-            },
-            agency: "Digital Management",
-            location: "Charlottesville, Virginia",
-            position: "Senior Frontend Developer - Javascript",
-            description: "DMI is a global, end-to-end mobility company that offers a variety of solutions including Mobility Strategy, Brand Marketing, UX, Web & App Development, Omnichannel Commerce, Big Data Insights, Managed Mobility Services, Cybersecurity Solutions and Infrastructure Services."
-          },
-          {
-            timeframe: {
-              start: "April 2011",
-              end: "November 2015"
-            },
-            agency: "Silverchair",
-            position: "Senior UI Developer",
-            location: "Charlottesville, Virginia",
-            description: "Silverchair designs and develops semantic technologies, publishing platforms, and e-learning solutions to scientific, technical and medical publishers, academic medical centers, professional societies, and the federal government in the United States."
-          },
-          {
-            timeframe: {
-              start: "April 2009",
-              end: "April 2011"
-            },
-            agency: "Enviroapps",
-            position: "UI Developer",
-            location: "Orlando, Florida",
-            description: "EnviroApps bring solutions to modern challenges using environment friendly technologies empowered by human imagination and care for the future generation. Their process of engineering development and deployment follow the GISTS (Green Infra Structure Technology Solutions) architecture to be eco-efficient and help transforming our engineers’ imaginative ideas in to products and solutions."
-          },
-        ]
-      }
-    },
-    methods: {
-      divideColumn(type){
-        var result = [[], []], halfLen = this.experience.length/2, pos = 0;
-        this.experience.forEach(function(val, i) {
-          if(type === 'odd-even') pos = i & 1;
-          else {
-            if(halfLen <= i) pos = 1;
-          }
 
-          result[pos].push(val);
-        })
-        return result;
-      },
-    },
-    created() {
-      this.experiencColumn = this.divideColumn()
-    }
+<script>
+// Shared Components
+import Header from '@/components/shared/Header'
+import Footer from '@/components/shared/Footer'
+
+// Pages
+import AboutContent from '@/components/pages/home/AboutContent'
+
+export default {
+  components: {
+    Footer,
+    Header,
+    AboutContent
   }
+}
 </script>
--- packages/smhello/src/components/pages/Blog.vue
+++ packages/smhello/src/components/pages/Blog.vue
@@ -1,25 +1,202 @@
 <template>
   <section class="layout">
     <Header />
-    <BlogContent />
+
+    <section class="page-header page-hero" style="background-image:url(../../../static/images/blog/blog-bg-01.jpg)">
+
+        <div class="row page-header__content">
+            <article class="col-full">
+
+                <div class="page-header__info">
+                    <div class="page-header__cat">
+                      <router-link
+                        v-for="(category, index) in posts[0].categories"
+                        :key="category.slug + '_' + index"
+                        :to="'/blog/' + category.slug">
+                        {{ category.name }}
+                      </router-link>
+                    </div>
+                    <div class="page-header__date">
+                        {{ posts[0].published | formatDate }}
+                    </div>
+                </div>
+
+                <h1 class="page-header__title">
+                    <h2 class="h01"><router-link :to="'/blog/' + posts[0].slug">{{ posts[0].title }}</router-link></h2>
+                </h1>
+                <p>{{ posts[0].summary }}</p>
+                <p>
+                    <router-link :to="'/blog/' + posts[0].slug" class="btn btn--stroke page-header__btn">Read more</router-link>
+                </p>
+            </article>
+        </div>
+
+    </section> <!-- end page-header -->
+
+    <section class="blog-content-wrap">
+
+      <div class="row blog-content">
+        <div class="blog-post__cat  col-full">
+
+          <h6>Categories</h6>
+          <a v-for="(category, index) in categories"
+            :key="category.slug + '_' + index"
+            @click="getPostsByCategory(category.slug)">
+            {{ category.name }}
+          </a>
+          <!-- <div class="row">
+            <div :class="'col-' + numberString[((categories.length > 4) ? 4 : 12/categories.length) - 1]"
+              v-for="(category, index) in categories"
+              :key="category.slug + '_' + index">
+              <a class="btn btn--stroke full-width" @click="getPostsByCategory(category.slug)">{{ category.name }}</a>
+            </div>
+          </div> -->
+        </div>
+          <div class="col-full">
+              <div class="blog-list block-1-2 block-tab-full">
+                  <article class="col-block" v-for="(post,index) in posts" :key="post.slug + '_' + index">
+                    <div class="blog-date">
+                        <a href="blog-single.html">{{ post.published | formatDate }}</a>
+                    </div>
+                    <h2 class="h01"><router-link :to="'/blog/' + post.slug">{{ post.title }}</router-link></h2>
+                    <p>{{ post.summary }}</p>
+                    <div class="blog-cat">
+                        <router-link
+                          v-for="(category, index) in post.categories"
+                          :key="category.slug + '_' + index"
+                          :to="'/blog/' + category.slug">
+                          {{ category.name }}
+                        </router-link>
+                    </div>
+                  </article>
+                </div>
+
+          </div> <!-- end col-full -->
+
+      </div> <!-- end blog-content -->
+
+        <div v-if="posts.length > 10" class="row blog-entries-nav">
+            <div class="col-full">
+                <a @click="getPosts(meta.previous_page)" class="btn btn--stroke">
+                    <i class="im im-arrow-left"></i>
+                    Prev
+                </a>
+
+                <a @click="getPosts(meta.next_page)" class="btn btn--stroke">
+                    Next
+                    <i class="im im-arrow-right"></i>
+                </a>
+            </div>
+        </div>
+
+    </section> <!-- end blog-content-wrap -->
+
     <Footer />
   </section>
 </template>
 
 
 <script>
+import { butter } from '@/buttercms'
+import moment from 'moment';
 // Shared Components
 import Header from '@/components/shared/Header'
 import Footer from '@/components/shared/Footer'
 
 // Pages
-import BlogContent from '@/components/pages/blog/BlogContent'
+import BlogContent from '@/components/pages/home/BlogContent'
 
 export default {
+  data () {
+    return {
+      numberString: ['one', 'two', 'three', 'four'],
+      posts: [{
+        categories: []
+      }],
+      meta: [],
+      categories: [],
+    }
+  },
+  methods: {
+    getPosts($page) {
+      butter.post.list({
+        page: $page,
+        page_size: 10
+      }).then((res) => {
+        //console.log(res.data.meta)
+        this.posts = res.data.data
+        this.meta = res.data.meta
+      })
+    },
+    getCategories() {
+      butter.category.list()
+        .then((res) => {
+          //console.log('List of Categories:')
+          //console.log(res.data.data)
+          this.categories = res.data.data
+        })
+    },
+    getPostsByCategory(category) {
+      butter.category.retrieve(category, {
+          include: 'recent_posts'
+        })
+        .then((res) => {
+          //console.log('Posts with specific category:')
+          //console.log(res.data)
+          this.posts = res.data.data.recent_posts
+        })
+    },
+  },
+  watch: {
+    $route() {
+      this.getPosts(1)
+    }
+  },
+  created() {
+    this.getPosts(1)
+    this.getCategories()
+  },
   components: {
     Footer,
     Header,
     BlogContent
+  },
+  filters: {
+    formatDate: function(value) {
+      if (value) {
+        return moment(String(value)).format('LL')
+      }
+    }
   }
 }
 </script>
+
+<style lang="scss">
+  .blog-post__cat {
+    display: inline-block;
+    margin-right: 2rem;
+    margin-bottom: .3rem;
+    font-size: 1.4rem;
+    line-height: 1.5;
+    text-transform: uppercase;
+    letter-spacing: .16rem;
+    color: rgba(255, 255, 255, 0.8);
+    font-family: "montserrat-bold", sans-serif;
+
+    a {
+      //color: white;
+      cursor: pointer;
+      &:hover, &:focus, &:active {
+        color: rgba(134, 33, 33, 0.5);
+      }
+      &::after {
+        content: ", ";
+      }
+      &:last-child::after {
+          display: none;
+      }
+    }
+  }
+  .blog-list article:nth-child(2n+1) { padding-left: 0; }
+  .blog-list article:nth-child(2n+2) { padding-right: 0; }
+</style>
