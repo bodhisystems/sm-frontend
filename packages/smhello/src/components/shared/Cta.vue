1974c19e3e263538694430a8b3fa91c2b8bd65e8
Subash Maharjan <subash.maharjan@updater.com>
2018-06-04 10:55:25 -0400

    blue host reference add added

--- packages/smhello/src/components/shared/Cta.vue
+++ packages/smhello/src/components/shared/Cta.vue
@@ -8,12 +8,15 @@
               <h2 class="h01"><a href="https://www.bluehost.com/home">Subash Maharjan Recommends Bluehost.</a></h2>
 
               <p class="lead">
+                <a href="https://www.bluehost.com/track/bodhisystems/" target="_blank"> <img border="0" src="https://bluehost-cdn.com/media/partner/images/bodhisystems/760x80/760x80BW.png"> </a>
               Looking for an awesome and reliable webhosting? Try <a href="https://www.bluehost.com/home">Bluehost</a>.
               <!-- Get <span>$50 off</span> when you sign up with the promocode <span>styleshout</span>. -->
               </p>
 
               <div class="cta__action">
-                  <a class="btn btn--primary btn--large" href="https://www.bluehost.com/home">Sign Up Now</a>
+
+
+                  <a class="btn btn--primary btn--large" href="https://www.bluehost.com/track/bodhisystems/">Sign Up Now</a>
               </div>
 
           </div>
