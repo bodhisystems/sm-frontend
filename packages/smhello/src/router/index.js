2046ec100a0485d83b14377b64e6b138a259a52a
Subash Maharjan <bodhisystems@gmail.com>
2018-06-25 13:45:59 -0400

    Fixing routing to new page but not scrolling to top of the page issue

--- packages/smhello/src/router/index.js
+++ packages/smhello/src/router/index.js
@@ -6,61 +6,83 @@ import About from '@/components/pages/About'
 import Works from '@/components/pages/Works'
 import Blog from '@/components/pages/Blog'
 import BlogPost from '@/components/pages/Blogpost'
+import Resume from '@/components/pages/Resume'
 import Contact from '@/components/pages/Contact'
 import BadRoute from '@/components/pages/BadRoute'
 import Styles from '@/components/pages/Styles'
 
 Vue.use(Router)
 
-export default (base, mode) =>
-  new Router({
-    base,
-    mode,
-    routes: [
-      {
-        path: '/',
-        name: 'siteHome',
-        component: Home
-      },
-      {
-        path: '/home',
-        name: 'home',
-        component: Home
-      },
-      {
-        path: '/about',
-        name: 'about',
-        component: About
-      },
-      {
-        path: '/works',
-        name: 'works',
-        component: Works
-      },
-      {
-        path: '/blog',
-        name: 'blog',
-        component: Blog
-      },
-      {
-        path: '/blog/:slug',
-        name: 'blog-post',
-        component: BlogPost
-      },
-      {
-        path: '/contact',
-        name: 'contact',
-        component: Contact
-      },
-      {
-        path: '*',
-        name: 'badRoute',
-        component: BadRoute
-      },
-      {
-        path: '/styles',
-        name: 'styles',
-        component: Styles
+export default (base, mode) => new Router({
+  base: "/",
+  mode: "history",
+  scrollBehavior: (to, from, savedPosition) => {
+    if (to.hash) {
+      return {
+        selector: to.hash
       }
-    ]
+    }
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return {x: 0, y: 0}
+    }
+  },
+  routes: [
+    {
+      path: '/',
+      name: 'siteHome',
+      component: Home
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '/about/:name',
+      name: 'aboutme',
+      component: About
+    },
+    {
+      path: '/works',
+      name: 'works',
+      component: Works
+    },
+    {
+      path: '/blog',
+      name: 'blog',
+      component: Blog
+    },
+    {
+      path: '/blog/:slug',
+      name: 'blog-post',
+      component: BlogPost
+    },
+    {
+      path: '/resume',
+      name: 'resume',
+      component: Resume
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
+    },
+    {
+      path: '*',
+      name: 'badRoute',
+      component: BadRoute
+    },
+    {
+      path: '/styles',
+      name: 'styles',
+      component: Styles
+    }
+  ]
 });
