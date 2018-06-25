ce28da8112f93af7a7364df710c3cc529092bfc0
Subash Maharjan <bodhisystems@gmail.com>
2018-06-25 13:31:27 -0400

    Navigation links and active link styling updated

--- packages/smhello/src/components/shared/Footer.vue
+++ packages/smhello/src/components/shared/Footer.vue
@@ -8,23 +8,23 @@
               </div>
 
               <ul class="footer-social">
-                  <li><a href="https://www.facebook.com/subash.maharjan.33">
+                  <li><a href="https://www.facebook.com/subash.maharjan.33" target="_blank">
                       <i class="im im-facebook" aria-hidden="true"></i>
                       <span>Facebook</span>
                   </a></li>
-                  <li><a href="https://twitter.com/submjn">
+                  <li><a href="https://twitter.com/submjn" target="_blank">
                       <i class="im im-twitter" aria-hidden="true"></i>
                       <span>Twitter</span>
                   </a></li>
-                  <li><a href="https://www.instagram.com/hudeomah/">
+                  <li><a href="https://www.instagram.com/hudeomah/" target="_blank">
                       <i class="im im-instagram" aria-hidden="true"></i>
                       <span>Instagram</span>
                   </a></li>
-                  <li><a href="https://www.behance.net/mr_submjn58b7">
+                  <li><a href="https://www.behance.net/mr_submjn58b7" target="_blank">
                       <i class="im im-behance" aria-hidden="true"></i>
                       <span>Behance</span>
                   </a></li>
-                  <li><a href="https://www.linkedin.com/in/subashmaharjan/">
+                  <li><a href="https://www.linkedin.com/in/subashmaharjan/" target="_blank">
                       <i class="im im-linkedin" aria-hidden="true"></i>
                       <span>LinkedIn</span>
                   </a></li>
