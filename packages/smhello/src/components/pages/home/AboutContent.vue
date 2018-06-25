4ae2cc661225c8ec1475fde0c39aaca306657675
Subash Maharjan <bodhisystems@gmail.com>
2018-06-25 13:45:12 -0400

    Implementing Vuex

--- packages/smhello/src/components/pages/home/AboutContent.vue
+++ packages/smhello/src/components/pages/home/AboutContent.vue
@@ -20,43 +20,13 @@
 
           </div>
 
-          <!-- <div class="col-six tab-full right">
-
-          </div> -->
-
-          <!-- <div class="col-six tab-full right">
-              <h3>I've Got Some skills.</h3>
-
-              <ul class="skill-bars">
-                  <li>
-                  <div class="progress percent90"><span>90%</span></div>
-                  <strong>HTML</strong>
-                  </li>
-                  <li>
-                  <div class="progress percent85"><span>85%</span></div>
-                  <strong>CSS</strong>
-                  </li>
-                  <li>
-                  <div class="progress percent70"><span>70%</span></div>
-                  <strong>Javascript</strong>
-                  </li>
-                  <li>
-                  <div class="progress percent95"><span>50%</span></div>
-                  <strong>PHP, JAVA, .NET</strong>
-                  </li>
-                  <li>
-                  <div class="progress percent75"><span>60%</span></div>
-                  <strong>Wordpress, Drupal, OrchardCMS, Liferay</strong>
-                  </li>
-              </ul>
-          </div> -->
-
       </div> <!-- end about-content -->
 
       <div class="row about-content about-content--buttons">
 
           <div class="col-six tab-full left">
-              <a href="https://www.linkedin.com/in/subashmaharjan/" class="btn btn--primary full-width">Download My CV</a>
+              <!-- <a href="https://www.linkedin.com/in/subashmaharjan/" class="btn btn--primary full-width">Download My CV</a> -->
+              <router-link class="btn btn--primary full-width" to="/resume">My Resume</router-link>
           </div>
           <div class="col-six tab-full right">
               <a href="mailto:hudeomah@gmail.com" class="btn full-width">Hire Me Now</a>
@@ -76,7 +46,7 @@
                   <div class="timeline__bullet"></div>
                   <div class="timeline__header">
                       <p class="timeline__timeframe">{{ data.timeframe.start }} - {{ data.timeframe.end }}</p>
-                      <h3><a :href="data.links">{{ data.agency }}</a></h3>
+                      <h3><a :href="data.links" target="_blank">{{ data.agency }}</a></h3>
                       <h5>{{ data.location }}</h5>
                       <h6>{{ data.position }}</h6>
                   </div>
@@ -92,58 +62,16 @@
 </template>
 
 <script>
+  import { mapState } from 'vuex';
 
   export default {
     data() {
       return {
-        experiencColumn: [[],[]],
-        experience: [
-          {
-            timeframe: {
-              start: "March 2018",
-              end: "Present"
-            },
-            agency: "Updater",
-            links: "http://www.updater.com/",
-            position: "Senior Frontend Engineer",
-            location: "Charlottesville, Virginia",
-            description: "Updater is a technology company that builds powerful tools to help people move. Updater's digital tools help people find and reserve trustworthy moving companies, connect home services, forward their mail, transfer utilities, update accounts, and much more – all in one easy-to-use platform."
-          },
-          {
-            timeframe: {
-              start: "December 2015",
-              end: "March 2018"
-            },
-            agency: "Digital Management",
-            links: "https://dminc.com/",
-            location: "Charlottesville, Virginia",
-            position: "Senior Frontend Developer - Javascript",
-            description: "DMI is a global, end-to-end mobility company that offers a variety of solutions including Mobility Strategy, Brand Marketing, UX, Web & App Development, Omnichannel Commerce, Big Data Insights, Managed Mobility Services, Cybersecurity Solutions and Infrastructure Services."
-          },
-          {
-            timeframe: {
-              start: "April 2011",
-              end: "November 2015"
-            },
-            agency: "Silverchair",
-            links: "https://www.silverchair.com/",
-            position: "Senior UI Developer",
-            location: "Charlottesville, Virginia",
-            description: "Silverchair designs and develops semantic technologies, publishing platforms, and e-learning solutions to scientific, technical and medical publishers, academic medical centers, professional societies, and the federal government in the United States."
-          },
-          {
-            timeframe: {
-              start: "April 2009",
-              end: "April 2011"
-            },
-            agency: "Enviroapps",
-            links: "http://www.enviroapps.com/",
-            position: "UI Developer",
-            location: "Orlando, Florida",
-            description: "EnviroApps bring solutions to modern challenges using environment friendly technologies empowered by human imagination and care for the future generation. Their process of engineering development and deployment follow the GISTS (Green Infra Structure Technology Solutions) architecture to be eco-efficient and help transforming our engineers’ imaginative ideas in to products and solutions."
-          },
-        ]
+        experiencColumn: [[],[]]
       }
+    },
+    computed: {
+      ...mapState('portfolio', { experience: state => state.portfolio.resume.experience })
     },
     methods: {
       divideColumn(type){
